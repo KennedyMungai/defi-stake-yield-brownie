29ba36a9fffa4be4633958478b0602c1164d37d4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-01-30 15:31:44 +0300

    Deleted some unused files

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { DAppProvider, Mainnet } from "@usedapp/core";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const config = {
+	readOnlyChainId: Mainnet.chainId,
+	readOnlyUrls: {
+		[Mainnet.chainId]: `https://mainnet.infura.io/v3/${
+			import.meta.env.VITE_INFURA_KEY
+		}`,
+	},
+};
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<DAppProvider config={config}>
+			<App />
+		</DAppProvider>
+	</StrictMode>,
+);
