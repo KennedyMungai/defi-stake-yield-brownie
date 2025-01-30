021e0d9f8d9e1a524a0b43fb482c3da8793d7fde
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-01-30 14:16:18 +0300

    A series of tests for the deployment scriot

---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,43 @@
+"""Test token farm test script"""
+import pytest
+from brownie import network, exceptions
+
+from scripts.deploy import deploy_token_farm_and_dapp_token
+from scripts.utils import (LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account,
+                           get_contract)
+
+
+def set_price_feed_contract():
+    # Arrange
+    """
+    Test that the price feed contract can be set for a given token, and that
+    only the owner of the contract can do so.
+    """
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local network testing")
+
+    account = get_account()
+    non_owner = get_account(_index=1)
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+
+    # Act
+    price_feed_address = get_contract("eth_price_feed_address")
+    token_farm.setPriceFeedContract(
+        dapp_token.address,
+        price_feed_address,
+        {
+            "from": account
+        }
+    )
+
+    # Assert
+    assert token_farm.tokenPriceFeedMapping[dapp_token.address] == price_feed_address
+
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.setPriceFeedContract(
+            dapp_token.address,
+            price_feed_address,
+            {
+                "from": non_owner
+            }
+        )
