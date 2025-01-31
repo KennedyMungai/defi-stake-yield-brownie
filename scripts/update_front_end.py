2d32a56002810aebacdb8b9fbde72146619dd32c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-01-31 08:34:21 +0300

    Running the logic to update the frontend configg

---
+++ scripts/update_front_end.py
@@ -0,0 +1,7 @@
+"""Function implementing the logic to update the frontend"""
+from scripts.deploy import update_front_end
+
+
+def main():
+    """The main function for the script"""
+    update_front_end()
